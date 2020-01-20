d91ec15ba5b24b4ba1bb6462d1dfb0ae8cb89dab
Rob Terrell <Rterrell25@gmail.com>
2020-01-20 13:16:31 -0500

    created an action file within the store folder and exported the actions I was previously hardcoding in order to dispatch.  now I am importing an object of actions and referencing this object as opposed to hardcoding

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
   counter: 0,
   results: []
@@ -5,32 +7,32 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case actionTypes.INCREMENT:
       return {
         ...state,
         counter: state.counter + 1
       };
-    case "DECREMENT":
+    case actionTypes.DECREMENT:
       return {
         ...state,
         counter: state.counter - 1
       };
-    case "ADD_COUNTER":
+    case actionTypes.ADD_COUNTER:
       return {
         ...state,
         counter: state.counter + action.value
       };
-    case "MINUS_COUNTER":
+    case actionTypes.MINUS_COUNTER:
       return {
         ...state,
         counter: state.counter - action.value
       };
-    case "STORE_RESULT":
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
-    case "DELETE_RESULT":
+    case actionTypes.DELETE_RESULT:
       const updatedArray = state.results.filter(
         result => result.id !== action.resultElId
       );
